e974c82434c3ceb19d0475438e1ca853d33b0310
Pierre-Emmanuel G <pierreemmanuelg@gmail.com>
2022-04-04 15:53:22 +0200

    Creation of forms for Article and Categories

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticlesController extends AbstractController
+class CategoryArticlesController extends AbstractController
 {
     #[Route('/articles', name: 'app_articles')]
     public function index(): Response
