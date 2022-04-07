14bf82b15ad3a699d21f94e8bb76bf0fd0a58aa2
Pierre-Emmanuel G <pierreemmanuelg@gmail.com>
2022-04-07 15:18:26 +0200

    Cart stored in DDB for each logged user

--- src/Storage/CartSessionStorage.php
+++ src/Storage/CartSessionStorage.php
@@ -3,30 +3,42 @@
 namespace App\Storage;
 
 use App\Entity\Order;
+use App\Entity\User;
 use App\Repository\OrderRepository;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Security;
 
 class CartSessionStorage
 {
     private RequestStack $requestStack;
     private OrderRepository $cartRepository;
+    private Security $security;
     const CART_KEY_NAME = 'cart_id';
 
-    public function __construct(RequestStack $requestStack, OrderRepository $cartRepository)
+    public function __construct(RequestStack $requestStack, OrderRepository $cartRepository, Security $security)
     {
         $this->requestStack = $requestStack;
         $this->cartRepository = $cartRepository;
+        $this->security = $security;
     }
     //TODO Add here the user ID to find the card ok the logged user
     //https://ourcodeworld.com/articles/read/46/check-if-signed-user-have-a-specific-role-in-symfony-2-3
     public function getCart(): ?Order
     {
         //TODO add check if the user is connected
-        return $this->cartRepository->findOneBy([
+        $cart = $this->cartRepository->findOneBy([
             'id' => $this->getCartId(),
             'status' => Order::STATUS_CART
         ]);
+
+        return $cart;
+    }
+
+    public function getCartUser($cartID): ?Order
+    {
+        $cart = $this->cartRepository->find($cartID);
+        return $cart;
     }
 
     //TODO Add here the userID to set the cart
