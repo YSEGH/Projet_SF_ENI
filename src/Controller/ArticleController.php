59502b2299e57ff0c3e9f95076e06ecb16a8c4d1
ysegh <youssef.segh@hotmail.fr>
2022-04-08 10:12:50 +0200

    cart test

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\AsciiSlugger;
 
-#[Route('/article')]
+#[Route('/mon-compte/articles')]
 class ArticleController extends AbstractController
 {
     #[Route('/', name: 'app_article_index', methods: ['GET'])]
